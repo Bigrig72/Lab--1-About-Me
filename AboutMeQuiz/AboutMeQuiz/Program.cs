1789e5fe1ee2e1481c37c1f7e11066eb355895e2
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-08 12:33:17 -0800

    finished guessing game-array working on Q5

--- AboutMeQuiz/AboutMeQuiz/Program.cs
+++ AboutMeQuiz/AboutMeQuiz/Program.cs
@@ -106,7 +106,7 @@ namespace AboutMeQuiz
 
                 uint citiesResponse = cityInfoGuess();
 
-                if (citiesResponse == 3)
+                if (citiesResponse == 5)
                 {
                     guesses++;
                     Console.WriteLine($"That is correct, {name}");
@@ -114,7 +114,33 @@ namespace AboutMeQuiz
                 }
                 else
                 {
-                    Console.WriteLine($"Sorry {name} we actually won a championship each year I was there! Very proud to be part of a winning team!");
+                    Console.WriteLine($"Sorry {name} I actually lived in 4 cities, Malone NY, Selinsgrove PA, Atlanta GA, Boone NC was where I went to school and Washington DC is my home");
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine($"Exception thrown: {e.Message}");
+
+            }
+            // Question 5
+            try
+            {
+
+                string twinsResponse = TwinsGuess();
+
+                if (twinsResponse == "correct")
+                {
+                    guesses++;
+                    Console.WriteLine($"That is correct, {name}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry {name} Their names are Colton and Brayden!");
                 }
 
 
@@ -130,6 +156,7 @@ namespace AboutMeQuiz
             {
                 Console.WriteLine($"Finished Quiz. Score: {guesses}");
             }
+
 
         }
 
@@ -194,14 +221,47 @@ namespace AboutMeQuiz
             string guesses = Console.ReadLine();
             int cityGuess = Convert.ToUInt16(guesses);
 
-            if (cityGuess != 3)
+            if (cityGuess != 5)
             {
                 return 1;
             }
             else
             {
-                return 3;
+                return 5;
             }
+        }
+
+        // Question 5 
+        static string TwinsGuess()
+        {
+            Console.WriteLine("Guess the two names of my twin boys?");
+
+            string[] nameArray = { "Colton", "Danny", "Brayden", "Stephen" };
+
+            foreach (string name in nameArray)
+            {
+                System.Console.WriteLine($"Names: {name}");
+            }
+
+            string twinGuess = Console.ReadLine();
+
+            string twinGuess2 = Console.ReadLine();
+
+
+            if ((twinGuess != nameArray[0] && twinGuess != nameArray[2]) && (twinGuess2 != nameArray[0] && twinGuess2 != nameArray[2]))
+            {
+                return "incorrect";
+            }
+            return "correct";
+           
+
+
+
+
+
+
+
+
         }
     }
 }
